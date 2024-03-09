cc81b3221b2819f95958824d6211bb23f39ab544
Dhruv Sheth <123584784+Dhruvnet@users.noreply.github.com>
2024-03-09 18:08:48 +0530

    Update index.jsx

--- src/components/room/index.jsx
+++ src/components/room/index.jsx
@@ -8,14 +8,14 @@ const RoomPage = () => {
         const myMeeting = async (element) => {
             const appID = 528380420;
             const serverSecret = "53c09f4415b9f5f272d8fdfe208f4a23";
-            const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(appID, serverSecret, roomId, Date.now().toString(), "Divye Shah");
+            const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(appID, serverSecret, roomId, Date.now().toString(), "Name");
             const zc = ZegoUIKitPrebuilt.create(kitToken);
             zc.joinRoom({
                 container: element,
                 sharedLinks: [
                     {
                         name: 'Copy Link',
-                        url: `http://localhost:3000/room/${roomId}`,
+                        url: `https://tensionflow-online.vercel.app/room/${roomId}`,
                     },
                 ],
                 scenario: {
